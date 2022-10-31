17406049204f38083fbedacf9d1524b47597f7dd
aliceb91 <alice.birkett@outlook.com>
2022-10-31 17:43:53 +0000

    Mongo compatibility added

--- database.js
+++ database.js
@@ -1,8 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb://admin:admin@localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256";
-
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.MONGO_URI);
 
 module.exports = {};
 module.exports.run = function run() {
