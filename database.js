fed9e843f3d4bdfa9b00a4cc4c6f80c09213bd42
aliceb91 <alice.birkett@outlook.com>
2022-11-12 17:37:21 +0000

    development improvement

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient(process.env.MONGO_URI || "mongodb://admin:admin@localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256");
 
 module.exports = {};
 module.exports.run = function run() {
